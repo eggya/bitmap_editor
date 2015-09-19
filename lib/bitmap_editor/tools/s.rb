fb92ecc5554a7afd870cd9629f34783b194e01f1
eggya <eggyachiquita@yahoo.com>
2015-09-19 17:09:01 +0100

    working show tool

--- lib/bitmap_editor/tools/s.rb
+++ lib/bitmap_editor/tools/s.rb
@@ -4,12 +4,14 @@ module BitmapEditor
 
       # returns [Boolean] True when validation passed
       def validated?
-        false
+        params.empty?
       end
 
       # returns [Boolean] True when succesfully performed
       def perform!
-        false
+        return unless validated? || bitmap.dimension.zero?
+        bitmap.pixels.values.inject("") {|str,row| str = str.to_s + (row.join + "\n") ; printf str } 
+        true
       end
       
     end
--- spec/lib/tools/s_spec.rb
+++ spec/lib/tools/s_spec.rb
@@ -3,33 +3,34 @@ require 'spec_helper'
 describe BitmapEditor::Tool::S do
 
   let(:params) { [] }
-  let(:bitmap) { BitmapEditor::Bitmap.new }
+  let(:bitmap) { BitmapEditor::Bitmap.new 3,3 }
+  before { bitmap.generate_pixels }
 
   #### CLASS METHODS
 
   describe "#Perform!" do
-    subject { BitmapEditor::Tool::Base.perform! bitmap,params }
+    subject { BitmapEditor::Tool::S.perform! bitmap,params }
 
-    it "should return false as default" do
-      expect( subject ).to be false
+    it "should return true as default" do
+      expect( subject ).to be true
     end
   end
 
   #### INSTANCE METHODS
 
   context "Instance methods" do
-    subject { BitmapEditor::Tool::Base.new bitmap,params }
-    it { is_expected.to be_instance_of BitmapEditor::Tool::Base }
+    subject { BitmapEditor::Tool::S.new bitmap,params }
+    it { is_expected.to be_instance_of BitmapEditor::Tool::S }
 
     describe "#Validated?" do
       it "should return false in base class" do
-        expect( subject.validated? ).to be false
+        expect( subject.validated? ).to be true
       end
     end
 
     describe "#Perform!" do
       it "should return false in base class" do
-        expect( subject.perform! ).to be false
+        expect( subject.perform! ).to be true
       end
     end
 
