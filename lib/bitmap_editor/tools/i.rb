e8384160dc53a5ff53d914e5fab995cd51a992a7
eggya <eggyachiquita@yahoo.com>
2015-09-19 16:11:29 +0100

    working create tool

--- lib/bitmap_editor/tools/i.rb
+++ lib/bitmap_editor/tools/i.rb
@@ -2,16 +2,28 @@ module BitmapEditor
   module Tool
     class I < Base
 
+      # initialize
+      # assign additional vars
+      def initialize bitmap,params
+        super
+        @x_axis = params[0].to_i
+        @y_axis = params[1].to_i
+      end
+
       # returns [Boolean] True when validation passed
       def validated?
-        false
+        params.count == 2
       end
 
       # returns [Boolean] True when succesfully performed
       def perform!
-        false
+        return unless validated?
+
+        bitmap.width  = @x_axis
+        bitmap.height = @y_axis
+        bitmap.pixels = bitmap.generate_pixels
       end
-      
+
     end
   end
 end
--- spec/lib/tools/i_spec.rb
+++ spec/lib/tools/i_spec.rb
@@ -2,34 +2,45 @@ require 'spec_helper'
 
 describe BitmapEditor::Tool::I do
 
-  let(:params) { ["M","N"] }
+  let(:params) { [3,4] }
   let(:bitmap) { BitmapEditor::Bitmap.new }
+  let(:new_bitmap) {{ 1 => [0, 0, 0], 2 => [0, 0, 0], 3 => [0, 0, 0], 4 => [0, 0, 0]}}
 
   #### CLASS METHODS
 
   describe "#Perform!" do
-    subject { BitmapEditor::Tool::Base.perform! bitmap,params }
+    subject { BitmapEditor::Tool::I.perform! bitmap,params }
 
-    it "should return false as default" do
-      expect( subject ).to be false
+    it "should return hash of arrays of the bitmap" do
+      expect{ subject }.to change{ [bitmap.width, bitmap.height, bitmap.pixels] }.to([3,4,new_bitmap])
     end
   end
 
   #### INSTANCE METHODS
 
   context "Instance methods" do
-    subject { BitmapEditor::Tool::Base.new bitmap,params }
-    it { is_expected.to be_instance_of BitmapEditor::Tool::Base }
+    subject { BitmapEditor::Tool::I.new bitmap,params }
+    it { is_expected.to be_instance_of BitmapEditor::Tool::I }
 
     describe "#Validated?" do
-      it "should return false in base class" do
-        expect( subject.validated? ).to be false
+
+      context "too many parameters" do
+        let(:params) { [1,2,3] }
+        it "should return false in non valid count of parameters" do
+          expect( subject.validated? ).to be false
+        end
+      end
+
+      context "passing validation" do
+        it "should return false in valid count of parameters" do
+          expect( subject.validated? ).to be true
+        end
       end
     end
 
     describe "#Perform!" do
-      it "should return false in base class" do
-        expect( subject.perform! ).to be false
+      it "should return hash of arrays of the bitmap" do
+        expect( subject.perform! ).to be_a Hash
       end
     end
 
