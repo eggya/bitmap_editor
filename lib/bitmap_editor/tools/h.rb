55bed6ecd007cad03b29a6348e4a2e8008711a8d
eggya <eggyachiquita@yahoo.com>
2015-09-19 17:39:51 +0100

    working horizontal colouring tool

--- lib/bitmap_editor/tools/h.rb
+++ lib/bitmap_editor/tools/h.rb
@@ -2,16 +2,41 @@ module BitmapEditor
   module Tool
     class H < Base
 
+      # initialize
+      # assign additional vars
+      def initialize bitmap,params
+        super
+        @x_axis_1 = params[0].to_i
+        @x_axis_2 = params[1].to_i
+        @y_axis   = params[2].to_i
+        @colour   = params[3].to_s
+      end
+
       # returns [Boolean] True when validation passed
       def validated?
-        false
+        valid_params? && valid_colour? && valid_dimension?
       end
 
       # returns [Boolean] True when succesfully performed
       def perform!
-        false
+        return unless validated?
+        [@x_axis_1,@x_axis_2].each {|i| bitmap.pixels[@y_axis][i-1] = @colour }
       end
-      
+
+      protected
+
+        def valid_params?
+          params.count == 4
+        end
+
+        def valid_colour?
+          !@colour.match(/^[A-Z]$/).nil?
+        end
+
+        def valid_dimension?
+          @x_axis_1 <= bitmap.width && @x_axis_2 <= bitmap.width && @y_axis <= bitmap.height
+        end
+
     end
   end
 end
--- spec/lib/tools/h_spec.rb
+++ spec/lib/tools/h_spec.rb
@@ -2,34 +2,69 @@ require 'spec_helper'
 
 describe BitmapEditor::Tool::H do
 
-  let(:params) { ["X1","X2","Y","C"] }
-  let(:bitmap) { BitmapEditor::Bitmap.new }
+  let(:params) { [1,3,3,"C"] }
+  let(:bitmap) { BitmapEditor::Bitmap.new 4,4 }
+
+  before do
+    bitmap.generate_pixels
+    bitmap.pixels[3][0] = "F"
+  end
 
   #### CLASS METHODS
 
   describe "#Perform!" do
-    subject { BitmapEditor::Tool::Base.perform! bitmap,params }
+    subject { BitmapEditor::Tool::H.perform! bitmap,params }
 
     it "should return false as default" do
-      expect( subject ).to be false
+      expect{ subject }.to change{ bitmap.pixels[3][0] }.from("F").to("C")
     end
   end
 
   #### INSTANCE METHODS
 
   context "Instance methods" do
-    subject { BitmapEditor::Tool::Base.new bitmap,params }
-    it { is_expected.to be_instance_of BitmapEditor::Tool::Base }
+    subject { BitmapEditor::Tool::H.new bitmap,params }
+    it { is_expected.to be_instance_of BitmapEditor::Tool::H }
 
     describe "#Validated?" do
-      it "should return false in base class" do
-        expect( subject.validated? ).to be false
+
+      context "validates params" do
+        let(:params) { [1,2,"C",3,4] }
+
+        it "should return false when too many parameters" do
+          expect( subject.send :valid_params? ).to be false
+          expect( subject.validated? ).to be false
+        end
+      end
+
+      context "validates colour" do
+        let(:params) { [1,2,3,4] }
+
+        it "should return false when not correct format of colour" do
+          expect( subject.send :valid_colour? ).to be false
+          expect( subject.validated? ).to be false
+        end
+      end
+
+      context "validates dimension" do
+        let(:params) { [4,5,1,"C"] }
+
+        it "should return false when coordinates are bigger than canvass" do
+          expect( subject.send :valid_dimension? ).to be false
+          expect( subject.validated? ).to be false
+        end
+      end
+
+      context "passing validation" do
+        it "should return true when validation satisfied" do
+          expect( subject.validated? ).to be true
+        end
       end
     end
 
     describe "#Perform!" do
       it "should return false in base class" do
-        expect( subject.perform! ).to be false
+        expect{ subject.perform! }.to change{ bitmap.pixels[3][0] }.from("F").to("C")
       end
     end
 
