8f05983320d916b71acea3ffb84d42d7bba2ce93
eggya <eggyachiquita@yahoo.com>
2015-09-19 17:40:22 +0100

    uncomment command spec

--- spec/lib/command_spec.rb
+++ spec/lib/command_spec.rb
@@ -17,8 +17,8 @@ describe BitmapEditor::Command do
     end
 
     context "case of valid command" do
-      # subject { BitmapEditor::Command.new bitmap,['I','M','N'] }
-      # it { is_expected.to be_instance_of BitmapEditor::Command }
+      subject { BitmapEditor::Command.new bitmap,['I','M','N'] }
+      it { is_expected.to be_instance_of BitmapEditor::Command }
     end
 
   end
@@ -26,22 +26,22 @@ describe BitmapEditor::Command do
   #### Instance Methods
 
   describe "#Initialize" do
-    # subject { BitmapEditor::Command.new bitmap,['I','M','N'] }
-    # it { is_expected.to be_instance_of BitmapEditor::Command }
+    subject { BitmapEditor::Command.new bitmap,['I','M','N'] }
+    it { is_expected.to be_instance_of BitmapEditor::Command }
 
-    # it "should assigns class variables" do
-    #   expect( subject.key ).to eq 'I'
-    #   expect( subject.params ).to be_a Array
-    #   expect( subject.params ).to match ['M','N']
-    # end
+    it "should assigns class variables" do
+      expect( subject.key ).to eq 'I'
+      expect( subject.params ).to be_a Array
+      expect( subject.params ).to match ['M','N']
+    end
   end
 
   describe "#Execute!" do
-    # subject { BitmapEditor::Command.new(bitmap,['I','M','N']).execute! }
+    subject { BitmapEditor::Command.new(bitmap,['I']).execute! }
 
-    # it "should return bitmap editor error" do
-    #   expect{ subject }.to raise_error BitmapEditor::ValidationError
-    # end
+    it "should return bitmap editor error when failing validation" do
+      expect{ subject }.to raise_error BitmapEditor::ValidationError
+    end
   end
 
 end
